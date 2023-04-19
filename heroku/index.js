5415f6d28f83f82544892a0ac74d7b05a89799b2
Zinab Abdulmanea <zabdulmanea@gmail.com>
2023-04-19 16:38:00 +0300

    reset the code to default

--- heroku/index.js
+++ heroku/index.js
@@ -20,12 +20,12 @@ app.use(bodyParser.json());
 var token = process.env.TOKEN || 'token';
 var received_updates = [];
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   console.log(req);
   res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>' + "hello");
 });
 
-app.get(['/facebook', '/instagram'], function(req, res) {
+app.get(['/facebook', '/instagram'], function (req, res) {
   if (
     req.query['hub.mode'] == 'subscribe' &&
     req.query['hub.verify_token'] == token
@@ -36,7 +36,7 @@ app.get(['/facebook', '/instagram'], function(req, res) {
   }
 });
 
-app.post('/facebook', function(req, res) {
+app.post('/facebook', function (req, res) {
   console.log('Facebook request body:', req.body);
 
   if (!req.isXHubValid()) {
@@ -48,15 +48,13 @@ app.post('/facebook', function(req, res) {
   console.log('request header X-Hub-Signature validated');
   // Process the Facebook updates here
   received_updates.unshift(req.body);
-  // let phone = received_updates[0].entry.changes[0].messages[0].from;
-  var phone = "";
-  console.log(received_updates[0].entry);
+
 
   res.sendStatus(200);
 
 });
 
-app.post('/instagram', function(req, res) {
+app.post('/instagram', function (req, res) {
   console.log('Instagram request body:');
   console.log(req.body);
   // Process the Instagram updates here
