7504adaa106a21a10782cdf674e5d19ea65aa012
Zinab Abdulmanea <zabdulmanea@gmail.com>
2023-04-19 13:44:22 +0300

    remove phone number

--- heroku/index.js
+++ heroku/index.js
@@ -22,7 +22,7 @@ var received_updates = [];
 
 app.get('/', function(req, res) {
   // console.log(req);
-  res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
+  res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>' + "hello");
 });
 
 app.get(['/facebook', '/instagram'], function(req, res) {
